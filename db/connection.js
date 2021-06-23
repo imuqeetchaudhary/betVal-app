c27a44565d6777eb67c1c7394ecded7bf296d513
Abdul Muqeet Arshad <abduliscoool@gmail.com>
2021-06-23 10:24:52 +0500

    updated db connection string

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose")
 //     useUnifiedTopology: true
 // })
 
-exports.connection = mongoose.connect("mongodb+srv://muqeet_chaudhary:Abdul6890060@cluster0.bqu75.mongodb.net/tour-guide-appDB?retryWrites=true&w=majority", {
+exports.connection = mongoose.connect("mongodb+srv://muqeet_chaudhary:Abdul6890060@cluster0.bqu75.mongodb.net/betVal?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
